f1f9a1844baac3f97b113722e223366534f4bcb6
Purifying12 <106816155+Purifying12@users.noreply.github.com>
2023-08-02 15:08:51 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -158,3 +158,30 @@ window.onclick = function(event) {
     modal5.style.display = "none";
   }
 }
+
+// Get the modal1
+var modal6 = document.getElementById("myModal6");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn6");
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close6")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal6.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal6.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal6) {
+    modal6.style.display = "none";
+  }
+}
